1062d890b4bca77a9b6c742b1967ab00317cb3f3
David J Forer <david.j.forer@gmail.com>
2021-10-06 22:48:02 +0900

    react responsive navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Nav,
+  NavLogo,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+
+const Navbar = () => {
+    return (
+        <>
+           <Nav>
+            <NavLogo to="/">
+                Logo
+            </NavLogo>
+            <Bars />
+
+            <NavMenu>
+                <NavLink to="/" activestyle>
+                    Home
+                </NavLink>
+                <NavLink to="/about" activestyle>
+                    About
+                </NavLink>
+                <NavLink to="/contact" activestyle>
+                    Contact
+                </NavLink>
+                <NavLink to="/signin" activestyle>
+                    Sign In
+                </NavLink>
+                <NavBtn>
+                    <NavBtnLink to="/sign-up">Sign Up</NavBtnLink>                
+                </NavBtn>
+            </NavMenu> 
+           </Nav> 
+        </>
+    );
+};
+export default Navbar;
